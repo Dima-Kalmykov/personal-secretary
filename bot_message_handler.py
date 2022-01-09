eaef6a79a917c00f42271ad00fc6971ef228cd01
Dima-Kalmykov <dekalmykov@edu.hse.ru>
2022-01-09 21:27:31 +0300

    Codestyle improvements

--- bot_message_handler.py
+++ bot_message_handler.py
@@ -14,14 +14,16 @@ bot = TeleBot(BOT_TOKEN)
 def revoke(message):
     user_id = message.from_user.id
     credentials = Credentials(**utils.get_google_credentials(user_id))
-    response = requests.post('https://oauth2.googleapis.com/revoke',
-                             params={'token': credentials.token},
-                             headers={'content-type': 'application/x-www-form-urlencoded'})
+
+    response = requests.post(
+        'https://oauth2.googleapis.com/revoke',
+        params={'token': credentials.token},
+        headers={'content-type': 'application/x-www-form-urlencoded'}
+    )
 
     status_code = response.status_code
     response_message = "Something went wrong"
-    print(status_code)
-    print(response)
+
     if status_code == 200:
         dao.delete_user(user_id)
         response_message = "Access successfully revoked!"
